bf8a97497f841ba96e7bff90ecd567afee36391d
gaspardsaves <166817933+gaspardsaves@users.noreply.github.com>
2025-05-17 23:24:48 +0200

    Update DB

--- zanimotrip.sql
+++ zanimotrip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 13 mai 2025 à 15:37
+-- Généré le : sam. 17 mai 2025 à 23:22
 -- Version du serveur : 8.0.42-0ubuntu0.22.04.1
 -- Version de PHP : 8.1.2-1ubuntu2.21
 
@@ -67,7 +67,7 @@ CREATE TABLE `extra` (
 INSERT INTO `extra` (`id`, `stage`, `title`, `price`) VALUES
 (1, 1, 'Visite avec un guide ', 500),
 (2, 2, 'Ciel ouvert avec vue sur les étoiles ', 300),
-(3, 3, 'baignade dans une source naturelle ', 75),
+(3, 3, 'Baignade dans une source naturelle ', 75),
 (4, 4, 'Accompagnement d\'un photographe animalier ', 100),
 (5, 5, 'Sortie en petit zodiac pour une exploration rapprochée', 450),
 (6, 6, 'Participation à une mini-expérience scientifique encadrée', 150),
@@ -77,14 +77,14 @@ INSERT INTO `extra` (`id`, `stage`, `title`, `price`) VALUES
 (10, 10, 'Escale sur une île déserte pour un pique-nique et baignade', 200),
 (11, 11, 'Observation des oiseaux migrateurs pendant l\'excursion', 100),
 (12, 12, 'Randonnée à travers la forêt tropicale avec pauses pour découvrir la biodiversité', 150),
-(13, 13, 'Séance photo avec un guide expert pour observer les pandas de près', 400),
-(14, 14, ' Nuit en bivouac sous les étoiles avec un guide local', 370),
+(13, 13, 'Séance photo avec les pandas', 400),
+(14, 14, 'Nuit en bivouac sous les étoiles avec un guide local', 370),
 (15, 15, 'Séance de yoga dans l’ambiance paisible de la forêt de bambous', 200),
 (16, 16, 'Balade féerique en calèche tirée par des licornes ', 450),
 (17, 17, 'Champagne à bord ', 270),
 (18, 18, 'Séance de cross dans les prairies magiques', 360),
 (19, 19, 'Nuit dans des lodges traditionnels Sherpa', 375),
-(20, 20, 'Veillée autour du feu avec les légendes de l’Himalaya', 50),
+(20, 20, 'En présence de légendes de l’Himalaya', 50),
 (21, 21, 'Excursion en hélicoptère au-dessus de l\'Everest', 450),
 (22, 22, 'Visite guidée du temple', 260),
 (23, 23, 'Exploration de grottes mystérieuses', 125),
@@ -98,23 +98,23 @@ INSERT INTO `extra` (`id`, `stage`, `title`, `price`) VALUES
 (31, 31, 'Découverte de la culture locale avec des arrêts dans des villages traditionnels', 375),
 (32, 32, 'Aider à la conduite des yaks dans les pâturages', 200),
 (33, 33, 'Visite de sites historiques et culturels le long du trajet', 350),
-(34, 34, ' arrêts pour observer les célèbres cabanes Wookiee', 400),
+(34, 34, 'Arrêts pour observer les célèbres cabanes Wookiee', 400),
 (35, 35, 'Rencontres avec des civilisations galactiques avancées', 650),
 (36, 36, 'Création de gadgets high-tech Wookiee', 150),
 (37, 37, 'Collecte de feuilles et de graines pour créer un carnet de nature', 175),
 (38, 38, 'Promenade avec les éléphants dans un environnement naturel', 350),
 (39, 39, 'Massage doux des éléphants après la baignade', 400),
 (40, 40, 'Exploration d\'autres animaux nocturnes à l’aide de la caméra thermique', 200),
-(41, 41, 'Observation des constellations avec un guide', 100),
+(41, 41, 'Observation des constellations avec un astronome', 100),
 (42, 42, 'Rencontre avec les soigneurs pour découvrir les soins apportés aux animaux', 375),
 (43, 43, 'Exploration de sentiers botaniques', 75),
 (44, 44, 'Arrêts baignade dans des piscines naturelles et sous des cascades', 275),
 (45, 45, 'Visite guidée d’une réserve communautaire', 200),
-(48, 1, 'accès aux tombeaux', 400),
-(49, 2, 'douches et toilettes privatives', 325),
-(50, 3, 'pause sous les palmiers avec service de thé local', 60),
+(48, 1, 'Accès aux tombeaux', 400),
+(49, 2, 'Douches et toilettes privatives', 325),
+(50, 3, 'Pause sous les palmiers avec service de thé local', 60),
 (51, 4, 'Visite commentée par un guide spécialiste de la faune', 150),
-(52, 5, 'Balade encadrée sur la banquise depuis le bateau', 350),
+(52, 5, 'Balade encadrée sur la banquise', 350),
 (53, 6, 'Rencontre avec des chercheurs pour échanger sur leurs travaux', 200),
 (54, 7, 'Safari au lever du soleil pour une lumière magique', 300),
 (55, 8, 'Guide spécialisé dans la faune et la flore de la région', 250),
@@ -149,9 +149,9 @@ INSERT INTO `extra` (`id`, `stage`, `title`, `price`) VALUES
 (84, 39, 'Séance photo avec les éléphants dans la rivière ', 300),
 (85, 40, 'Pause dans un abri pour observer les renards de plus près', 250),
 (86, 41, 'Pause autour d\'un feu de camp en pleine forêt enneigée', 170),
-(87, 42, 'Observation des animaux en cours de réhabilitation depuis des plateformes discrètes', 315),
+(87, 42, 'Observation des animaux en cours de rémission depuis des plateformes discrètes', 315),
 (88, 43, 'Observation des indris avec un guide', 180),
-(89, 44, ' Nuit en bivouac sur une plage de sable au bord du fleuve', 400),
+(89, 44, 'Nuit en bivouac sur une plage de sable au bord du fleuve', 400),
 (90, 45, 'Excursion d’observation des baleines avec un guide spécialisé', 475);
 
 -- --------------------------------------------------------
@@ -186,10 +186,10 @@ CREATE TABLE `stage` (
 --
 
 INSERT INTO `stage` (`id`, `text`, `travel`, `chronology`, `image`) VALUES
-(1, 'Visite d\'une pyramide avec un guide ', 1, 1, 'img/trav/Pyramides-de-Gizeh.jpg'),
-(2, 'Nuit sous une Tente de luxe', 1, 2, 'img/trav/nuitTenteDesert.jpg'),
+(1, 'Visite d\'une pyramide', 1, 1, 'img/trav/Pyramides-de-Gizeh.jpg'),
+(2, 'Nuit sous une tente de luxe', 1, 2, 'img/trav/nuitTenteDesert.jpg'),
 (3, 'Exploration des oasis', 1, 3, 'img/trav/exploOasis.jpg'),
-(4, 'Observation des pingouins.', 2, 1, 'img/trav/observationPingouin.jpg'),
+(4, 'Observation des pingouins', 2, 1, 'img/trav/observationPingouin.jpg'),
 (5, 'Excursions en bateau', 2, 2, 'img/trav/excursionBateau.jpg'),
 (6, 'Visites de stations scientifiques', 2, 3, 'img/trav/stationScientifiques.jpeg'),
 (7, 'Safari en jeep', 3, 1, 'img/trav/safariJeep.jpeg'),
@@ -205,7 +205,7 @@ INSERT INTO `stage` (`id`, `text`, `travel`, `chronology`, `image`) VALUES
 (17, 'Vol en montgolfière au-dessus des prairies arc-en-ciel ', 6, 2, 'img/trav/montgolfiere.jpeg'),
 (18, 'Équitation avec les licornes', 6, 3, 'img/trav/EquitationLicorne.jpeg'),
 (19, 'Trekking au Mont Everest avec un guide Sherpa', 7, 1, 'img/trav/trekkingEverest.jpeg'),
-(20, 'Veillée autour du feu avec les légendes de l’Himalaya', 7, 2, 'img/trav/veilleeFeu.jpeg'),
+(20, 'Veillée autour du feu', 7, 2, 'img/trav/veilleeFeu.jpeg'),
 (21, 'Excursion en hélicoptère', 7, 3, 'img/trav/helicoEverest.jpeg'),
 (22, 'Exploration du Temple du Dragon', 8, 1, 'img/trav/TempleDragon.jpeg'),
 (23, 'Randonnée dans la Vallée des Dragons', 8, 2, 'img/trav/ValleeDragon.jpeg'),
@@ -227,7 +227,7 @@ INSERT INTO `stage` (`id`, `text`, `travel`, `chronology`, `image`) VALUES
 (39, 'Baignade avec les éléphants dans la rivière de Chiang Mai', 13, 3, 'img/trav/BaignadeElephants.jpeg'),
 (40, 'Observation des renards à la caméra thermique', 14, 1, 'img/trav/observationRenards.jpeg'),
 (41, 'Randonnée au crépuscule dans les forêts enneigées', 14, 2, 'img/trav/randoFinlande.jpeg'),
-(42, 'Visite d’un centre de réhabilitation de la faune nordique', 14, 3, 'img/trav/centreFinlande.jpeg'),
+(42, 'Visite d’un centre de soins de la faune nordique', 14, 3, 'img/trav/centreFinlande.jpeg'),
 (43, 'Randonnée dans le parc national d\'Andasibe-Mantadia ', 15, 1, 'img/trav/randoMada.jpeg'),
 (44, 'Croisière sur le fleuve Tsiribihina', 15, 2, 'img/trav/croisiereMada.jpeg'),
 (45, 'Visite des réserves naturelles de l\'île Sainte-Marie', 15, 3, 'img/trav/reserveMada.jpeg');
@@ -292,12 +292,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `lastname`, `firstname`, `email`, `password`, `role`) VALUES
-(1, 'Maslanka', 'Jean-Luc', 'maslankaje@cytech.fr', '$2y$12$xgNWFok2a8rNzr5VUVL.5eXNQqzphrcW/TcsWAxOvAZv9vl7uu9LS', 0),
-(2, 'Saves', 'Gaspard', 'gs@gmail.com', '$2y$12$f0.ITdKMgYLplWAQX98I6e1LYRpmr9TmYMHZqiFhjKEIodgupVxoa', 0),
-(3, 'Stashenko', 'Albina', 'AS@gmai.com', '$2y$12$HnniYl8JQgq05n8WuwFcreyK97YOh264tIL6Ilgaz5rN/vDQJrUrC', 1),
-(4, 'Trump', 'Donald', 'DonaldTrump@gmail.com', '$2y$12$9w9V5wG/pa6REDXXFzbCDO9p/GlxzU8Cm84TGxIUX6NA3LnIrkLwG', 1),
-(5, 'Banica', 'Teodor', 'TB@gmail.com', '$2y$12$3.NkSlqdQwgNM6NnJjxR3eW2d2IaGMuSfx4zAe1PjsbEa371kST/e', 1),
-(6, 'Saves', 'Gaspard', 'saves.gaspard@gmail.com', '$2y$12$YwmRB6E8DVHfMM.Qupv8CO5DeetFfbPBOE61Zcb3sw3fw4P1EqOBy', 1);
+(1, 'Saves', 'Gaspard', 'saves.gaspard@gmail.com', '$2y$12$ae681KhX7bsDOuYIFZYkp.XCp.rL.8vrpJ8/vstyt9uWTMR8Dmox2', 1);
 
 --
 -- Index pour les tables déchargées
@@ -383,7 +378,7 @@ ALTER TABLE `travel`
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
